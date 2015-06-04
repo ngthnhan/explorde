ae1fd3a39459f9693cb25ba9458b75907b833e28
ngthnhan <ngthnhan93@gmail.com>
2015-06-04 23:21:50 +0800

    Finished exploding functionality

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,40 +1,35 @@
 $(document).on("ready, page:change", function() {
 	var TWO_PI = 2 * Math.PI;
 
-	var MAX_LEVEL = $("#resolution_level").data("resolution-level") - 1;
+	var MAX_LEVEL = $("#main-canvas").data("resolution-level") - 1;
 	var MAX_RESOLUTION = Math.pow(2, MAX_LEVEL);
 
 	var SIZE = 512;
 	// Must be divisible
 	var STEP = SIZE / MAX_RESOLUTION;
-	var maskArrays = new Array(MAX_LEVEL);
-	initializeMaskArrays();
+	var maskArray = [];
+	initializeMaskArray();
 
-	function initializeMaskArrays() {
-		for (var lvl=0; lvl <= MAX_LEVEL; lvl++) {
-			var level = [];
-			for (var r=0; r < MAX_RESOLUTION; r++) {
-				var row_cells = [];
-				for (var c=0; c < MAX_RESOLUTION; c++) {
-					row_cells.push(lvl); // Start at level 0
-				}
-				level.push(row_cells.slice(0, row_cells.length));
+	function initializeMaskArray() {
+		for (var r=0; r < MAX_RESOLUTION; r++) {
+			var row_cells = [];
+			for (var c=0; c < MAX_RESOLUTION; c++) {
+				row_cells.push(0); // Start at level 0
 			}
-			maskArrays[lvl] = level.slice(0, level.length);
+			maskArray.push(row_cells.slice(0, row_cells.length));
 		}
-		console.log(maskArrays);
+		// console.log(maskArray);
 	}
 
-	function basicCell(level, r, c) {
+	function basicCell(r, c) {
 		this.row = r;
 		this.col = c;
 		this.getLevel = function() {
-			// console.log(level + ": " + this.row + " x " + this.col);
-			return maskArrays[level][this.row][this.col];
+			return maskArray[this.row][this.col];
 		}
 	}
 
-	function mousePos(level, x, y) {
+	function mousePos(x, y) {
 		this.x = x;
 		this.y = y;
 		this.getCell = function() {
@@ -42,64 +37,99 @@ $(document).on("ready, page:change", function() {
 			var row = ~~(this.x / STEP);		
 			var col = ~~(this.y / STEP);
 
-			return new basicCell(level, row, col);
+			return new basicCell(row, col);
 		};
 	}
 
-	function isInContact(level, pos) {
+	function canExplode(pos) {
 		var cell = pos.getCell();
 		var lvl = cell.getLevel();
 		
 		var cur_r = ~~(cell.col / (MAX_RESOLUTION / Math.pow(2, lvl)));
 		var cur_c = ~~(cell.row / (MAX_RESOLUTION / Math.pow(2, lvl)));
 
-		console.log("RxC: " + cur_r + ", " + cur_c);
 		// pos is in pixel_matrices[lvl][cur_r][cur_c]
 		var radius = SIZE / (Math.pow(2, lvl + 1));
 		var x = (2 * cur_c + 1) * radius;
 		var y = (2 * cur_r + 1) * radius;
 		
-		console.log("X, Y: " + x + "," + y);
-		console.log("Radius: " + radius);
 		var isTouched = (x-pos.x)*(x-pos.x) + (y-pos.y)*(y-pos.y) <= radius*radius;
-		
-		console.log("Level: " + level + "\nMouse position: " + pos.x + " x " + pos.y + "\nTouch: " + isTouched);
+		return isTouched && lvl < MAX_LEVEL;	
+		// console.log("Level: " + level + "\nMouse position: " + pos.x + " x " + pos.y + "\nTouch: " + isTouched);
 	}
 
-	function draw(level) {
-		var canvas = $("#level" + level);
-		var level_matrix = canvas.data("pixel-matrix");
+	function draw() {
+		var canvas = $("#main-canvas");
+		var pixelMatrices = canvas.data("pixel-matrices");
+		var red = 0, green = 1, blue = 2;
 
+		// Initial draw
+		if (canvas[0] && canvas[0].getContext) {
+			var ctx = canvas[0].getContext("2d");
+			var rad = SIZE / 2;
+			ctx.beginPath();
+			ctx.moveTo(rad, 0);
+			ctx.arc(rad, rad, rad, 0, TWO_PI);
+			var pixel = pixelMatrices[0][0][0];
+			ctx.closePath();
+			ctx.fillStyle = "rgb(" + Math.floor(pixel[red]) + "," + Math.floor(pixel[green]) + "," + Math.floor(pixel[blue]) + ")";
+			ctx.fill();
+		} else {
+			return;
+		}
+
+		// Add event listener
 		canvas[0].addEventListener('mousemove', function(e) {
 			var rect = canvas[0].getBoundingClientRect();
 			var x = Math.max(e.clientX - rect.left, 0);
 			var y = Math.max(e.clientY - rect.top, 0);
-			var pos = new mousePos(level, x, y);
-			isInContact(level, pos);
+			var pos = new mousePos(x, y);
+			if (canExplode(pos)) {
+				explode(pos.getCell());
+			}
 		});
 
-		if (canvas[0].getContext) {
-			var ctx = canvas[0].getContext("2d");
-			var size = 512;
-			var radius = size / (Math.pow(2, level + 1));
-			var red = 0, green = 1, blue = 2;
-			for (var row = 0; row < Math.pow(2, level); row++) {
-				for (var col = 0; col < Math.pow(2, level); col++) {
-					var pixel = level_matrix[row][col];
+		function explode(cell) {
+			var lvl = cell.getLevel();
+
+			// Update maskArray
+			var factor = MAX_RESOLUTION / Math.pow(2, lvl);
+			var starting_row = ~~(cell.row / factor);
+			var starting_col = ~~(cell.col / factor);
+			for (var r = starting_row * factor; r < (starting_row + 1) * factor; r++) {
+				for (var c = starting_col * factor; c < (starting_col + 1) * factor; c++) {
+					maskArray[r][c] += 1;
+				}
+			}
+
+			// Update graphic
+			// Remove old bubble
+			// Adding new bubble
+			var size_factor = SIZE / Math.pow(2, lvl);
+			var starting_x = starting_row * size_factor;
+			var starting_y = starting_col * size_factor;
+			console.log(starting_x + "," + starting_y + "," + size_factor + "," + size_factor);
+			ctx.clearRect(starting_x, starting_y, size_factor, size_factor);
+			
+			var row, col;
+			var radius = size_factor / 4;
+			for (var i = 0; i < 2; i++) {
+				col = 2 * starting_row + i;
+				for (var j = 0; j < 2; j++) {
+					row = 2 * starting_col + j; 
+					console.log("Next: " + (lvl + 1) + " - " + row + "," + col);
+					var pixel = pixelMatrices[lvl+1][row][col];
 					ctx.beginPath();
 					ctx.moveTo((2 * col + 1) * radius, row * radius);
 					ctx.arc((2 * col + 1) * radius, (2 * row + 1) * radius, radius, 0, TWO_PI);
 					var rgb = "rgb(" + Math.floor(pixel[red]) + "," + Math.floor(pixel[green]) + "," + Math.floor(pixel[blue]) + ")";
+					ctx.closePath();
 					ctx.fillStyle = rgb;
 					ctx.fill();
 				}
 			}
-		} else {
-			return;
 		}
 	}
 
-	for (var cvsLevel = 0; cvsLevel <= MAX_LEVEL; cvsLevel++) {
-		draw(cvsLevel);
-	}
+	draw();
 });
