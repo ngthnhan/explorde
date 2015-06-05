074caad80175b01ad33981b6a405003f29eae5c1
ngthnhan <ngthnhan93@gmail.com>
2015-06-06 04:06:27 +0800

    Config authenticate to actions except show and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   require 'RMagick'
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:show, :index]
 
   # GET /posts
   # GET /posts.json
