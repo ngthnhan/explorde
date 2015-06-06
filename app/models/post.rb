78b9ef4c72a7945705296c60823606643e7b3b57
Tran Manh Tu <xxlaguna93@gmail.com>
2015-06-07 00:47:44 +0800

    Add voting feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+	acts_as_votable
+	belongs_to :user
 	has_attached_file :image, styles: { :medium => "512x512#", :thumb => "100x100#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :resolution_level, :title, presence: true
