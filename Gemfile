ebb6dcc78f362f82032aad2c5f78accfb6d1bcca
ngthnhan <ngthnhan93@gmail.com>
2015-06-10 19:09:52 +0800

    Added structure to User show and switch to font-awesome

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'devise', '~> 3.5.1'
 gem 'simple_form'
 gem 'foundation-rails'
 gem 'friendly_id', '~> 5.1.0'
+gem 'font-awesome-rails'
 
 gem 'rails', '4.2.1'
 gem 'sqlite3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def display_rating(post)
+		rating = post.get_upvotes.size - post.get_downvotes.size
+		thumb = rating >= 0 ? "thumbs-o-up" : "thumbs-o-down"
+
+		fa_icon thumb, text: rating
+	end
 end
